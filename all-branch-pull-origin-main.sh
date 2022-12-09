3788387414db4f1e88c10f399c645ae3f01b88a8
gigo96 <sch102403@gmail.com>
2022-12-09 15:07:37 +0900

    파라미터로 입력 된 문자열이 현재 디렉토리인지 확인하는 것 추가

--- all-branch-pull-origin-main.sh
+++ all-branch-pull-origin-main.sh
@@ -4,10 +4,18 @@ if [ $# -ne 1 ];
   then
     echo "error: expected only 1 parameter(file name)"
     sleep 0.1
-    return 1
+    return 0
 fi
 
 j $1
+
+PWD=$(pwd -P)
+
+if [ ${#1} -gt ${#PWD} ] || [ ${PWD: -${#1}} != $1 ];then
+  echo "error: Enter a valid path"
+  sleep 0.1
+  return 0
+fi
 
 git checkout main && git pull origin main
 
